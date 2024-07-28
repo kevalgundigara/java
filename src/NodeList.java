d2220d653b48074f10b4b34b6b4de6f4d2aaccaf
crazydeveloper-keval <keval.crazydeveloper@gmail.com>
2024-07-28 20:13:32 +0530

    first commit

---
+++ src/NodeList.java
@@ -0,0 +1,8 @@
+// NodeList
+public interface NodeList {
+
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
